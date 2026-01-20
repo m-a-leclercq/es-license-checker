622541c2daffc4bc483ee4f6b29d4223cd2a1c1a
Marc-Antoine LECLERCQ <marc-antoine.leclercq@outlook.com>
2026-01-20 13:57:29 +0100

    Adding Ram calculations

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 This python script allows elasticsearch customers to get an accurate inventory of how many licenses their clusters are using for platinum licenses.
 
 It can also be used to estimate how many platinum licenses your basic cluster would require.
- 
-*This script will not work for Enterprise customers as it calculates nodes and not Enterprise Resource Units.*
+
+This script will give an approximate count of Enterprise Resource Units usage (ERU for Enterprise licensing).
 
 # Installation
 Create a new virtualenv
@@ -29,9 +29,12 @@ python main.py --es-url https://localhost:9200
 Each time you run the script and as long as you have queried more than one cluster, a `consolidated_license_usage.yaml` file will be created / updated with the list of `.yml` files that were inspected and the total number of billable nodes is then calculated.
 
 # CHANGELOG
+## v0.3.0
+- Allows all licences to be counted, and checks whether platinum is capped by nodes or RAM. 
+
 ## v0.2.2
 - Now only allows platinum, gold(discontinued) and basic licenses to be counted
-  
+
 ## v0.2.1
 - Wording changed for better clarity.
 
--- main.py
+++ main.py
@@ -1,50 +1,77 @@
 import click
+import glob
+import math
 import requests
 import yaml
-import os
+
+import humanfriendly
+from pathlib import Path
+
+# Billable roles per Elasticsearch license terms
+BILLABLE_ROLES = {"data", "data_hot", "data_warm", "data_cold", "data_content", "ml", "master"}
+GB_DIVISOR = humanfriendly.parse_size("1GB", binary=True)
+
+def convert_size_to_bytes(size_str):
+    """Converts a size string (e.g. '1gb', '512mb') to bytes."""
+    try:
+        if not size_str:
+            return 0
+        return humanfriendly.parse_size(str(size_str), binary=True)
+    except Exception:
+        return 0
 
 def dump_cluster_license(es_url, user, password, ca, insecure):
-    """This function takes 3 arguments as input: an url, a user, and a password."""
-    # set tls parameters for queries
-    if insecure:
-        verification = False
-    else :
-        verification = ca
+    """This function connects to an Elasticsearch cluster and dumps license and node information to a YAML file."""
     # Create a session object to keep the connection alive.
     session = requests.Session()
-
-    # Add the user and password to the session.
     session.auth = (user, password)
+    # Use system CA bundle (True) by default, custom CA if provided, or disable verification if insecure
+    session.verify = False if insecure else (ca or True)
 
     # Make a request to the _license and _nodes endpoint.
-    response = session.get(f"{es_url}/_license", verify=verification)
+    response = session.get(f"{es_url}/_license")
+    response_nodes = session.get(f"{es_url}/_nodes")
 
-    # Check for license type before proceeding any further.
-    if response.json()["license"]["type"] not in ["platinum", "basic", "gold"]:
-        print("License types other than basic or platinum are not supported")
-        exit(1)
-
-    response_nodes = session.get(f"{es_url}/_nodes", verify=verification)
-
+    # Query cat nodes for RAM info
+    response_cat = session.get(f"{es_url}/_cat/nodes?v=true&h=name,ram.max&s=name&format=json")
+    ram_map = {n['name']: convert_size_to_bytes(n.get('ram.max')) for n in response_cat.json()}
 
     # Get the cluster name from the _nodes response.
     cluster_name = response_nodes.json()["cluster_name"]
+    license_type = response.json()["license"]["type"]
 
-   # Create a dictionary with the results of both API calls.
+    nodes_dict = {}
+    for node in response_nodes.json()["nodes"].values():
+        node_name = node["name"]
+        node_ram = ram_map.get(node_name, 0)
+        is_billable = bool(BILLABLE_ROLES & set(node["roles"]))
+        nodes_dict[node_name] = {
+            "roles": node["roles"],
+            "counts_against_license": int(is_billable),
+            "ram.max": node_ram
+        }
+
+    billable_count = sum(n["counts_against_license"] for n in nodes_dict.values())
+    total_ram = sum(n["ram.max"] for n in nodes_dict.values())
+
+   # Create a dictionary with the results of all API calls.
     data = {
-        "billable nodes in the cluster": sum(1 if any(role in node["roles"] for role in ["data", "data_hot", "data_warm", "data_cold", "data_content", "ml", "master"]) else 0 for node in response_nodes.json()["nodes"].values()),
         "license": response.json()["license"],
         "nodes": {
             "_nodes": response_nodes.json()["_nodes"],
-            "nodes": {
-                node["name"]: {
-                    "roles": node["roles"],
-                    "counts_against_license": 1 if any(role in node["roles"] for role in ["data", "data_hot", "data_warm", "data_cold", "data_content", "ml", "master"]) else 0
-                }
-                for node in response_nodes.json()["nodes"].values()
-            }
+            "nodes": nodes_dict
         }
     }
+
+    total_ram_gb = total_ram / GB_DIVISOR
+
+    if license_type in ["platinum", "gold", "basic", "trial"]:
+        # Logic from subscription agreement:
+        # Billable Nodes = max(Role-based Nodes Count, ceil(Total RAM GB / 64))
+        ram_billable_count = math.ceil(total_ram_gb / 64)
+        data["billable nodes in the cluster"] = max(billable_count, ram_billable_count)
+    else:
+        data["Total amount of RAM consumed"] = round(total_ram_gb, 2)
 
    # Write the data to a YAML file.
     with open(f"{cluster_name}.yml", "w") as f:
@@ -52,37 +79,45 @@ def dump_cluster_license(es_url, user, password, ca, insecure):
 
 def consolidate_cluster_info():
     """This function looks for all .yml files at the root of the current directory and creates a new yml file called "consolidated_license_usage.yaml"."""
-
-    # Get a list of all the files in the current directory.
-    files = os.listdir()
-
-    # Filter the list of files to only include .yml files.
-    yml_files = [file for file in files if file.endswith(".yml")]
-    if len(yml_files) <= 1:
-        exit(0)
+    yml_files = glob.glob("*.yml")
 
     # Create a dictionary to store the consolidated license usage.
     consolidated_license_usage = {
         "list of audited clusters": [],
-        "Total number of billable nodes": 0
+        "Total number of billable nodes": 0,
+        "Total amount of RAM consumed": 0
     }
 
     # Loop through the .yml files and add their data to the consolidated license usage dictionary.
     for file in yml_files:
         # Open the .yml file.
         with open(file, "r") as f:
-            # Load the data from the .yml file.
-            data = yaml.safe_load(f)
+            try:
+                # Load the data from the .yml file.
+                data = yaml.safe_load(f)
 
-            # Add the basename of the .yml file to the list of audited clusters.
-            consolidated_license_usage["list of audited clusters"].append(os.path.splitext(os.path.basename(file))[0])
+                if not data:
+                    continue
 
-            # Add the billable nodes in the cluster to the total number of licenses used.
-            consolidated_license_usage["Total number of billable nodes"] += data["billable nodes in the cluster"]
+                # Add the basename of the .yml file to the list of audited clusters.
+                consolidated_license_usage["list of audited clusters"].append(Path(file).stem)
+
+                # Add totals
+                if "billable nodes in the cluster" in data:
+                    consolidated_license_usage["Total number of billable nodes"] += data["billable nodes in the cluster"]
+
+                if "Total amount of RAM consumed" in data:
+                    consolidated_license_usage["Total amount of RAM consumed"] += data["Total amount of RAM consumed"]
+            except Exception:
+                continue
+
+    # Round the Total amount of RAM consumed
+    consolidated_license_usage["Total amount of RAM consumed"] = round(consolidated_license_usage["Total amount of RAM consumed"], 2)
 
     # Write the consolidated license usage to a YAML file.
-    with open("consolidated_license_usage.yaml", "w") as f:
-        yaml.dump(consolidated_license_usage, f, default_flow_style=None)
+    if consolidated_license_usage["list of audited clusters"]:
+        with open("consolidated_license_usage.yaml", "w") as f:
+            yaml.dump(consolidated_license_usage, f, default_flow_style=None)
 
 @click.command()
 @click.option("--es-url", prompt="Elasticsearch URL", default="", help="The URL of the Elasticsearch instance.")
@@ -98,7 +133,7 @@ def main(es_url, user, password, ca, insecure, gather):
     It will tell you which nodes in your cluster count against your license and report the total number used by the cluster.\n
     Everytime you run the script against a new cluster, the consolidated license usage file is updated with the new information.\n
 
-    You can also run the script with -g / --gather to consolidate existing files without querying a cluster.  
+    You can also run the script with -g / --gather to consolidate existing files without querying a cluster.
     """
     if not gather:
         dump_cluster_license(es_url, user, password, ca, insecure)
